72f95bae43afe6e6dd81e42cd479ce753ae185d6
Andrei Ciocoiu <andreiciocoiu2002@icloud.com>
2024-03-07 23:59:48 +0000

    service.worker

---
+++ web-app copy/service-worker.js
@@ -0,0 +1,14 @@
+var cacheName = 'lessons-v1';
+var cacheFiles = [
+    "index.html"
+];
+
+self.addEventListener('install', function(e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
